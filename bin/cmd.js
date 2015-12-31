86d82b6f9acba8a50ec3e9c94cc2ad0efe81e8ee
Denys Dovhan <email@denysdovhan.com>
2015-12-31 13:53:39 +0200

    Add markdown highlighting.

--- bin/cmd.js
+++ bin/cmd.js
@@ -2,5 +2,11 @@
 
 var fs = require('fs');
 var pager = require('default-pager');
+var obj = require('through2').obj;
+var msee = require('msee');
 
-fs.createReadStream(__dirname + '/../readme.markdown').pipe(pager());
+fs.createReadStream(__dirname + '/../readme.markdown')
+  .pipe(obj(function (chunk, enc, cb) {
+    this.push(msee.parse(chunk.toString()))
+  }))
+  .pipe(pager());
