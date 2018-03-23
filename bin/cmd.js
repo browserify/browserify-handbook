31ed48daa0a8940e737b84c1d79bf547117b8c65
Renée Kooi <renee@kooi.me>
2018-03-23 09:48:42 +0100

    msee → marked (better at lists)

--- bin/cmd.js
+++ bin/cmd.js
@@ -2,11 +2,22 @@
 
 var fs = require('fs');
 var pager = require('default-pager');
-var obj = require('through2').obj;
-var msee = require('msee');
+var through = require('through2');
+var duplexer = require('duplexer2');
+var concat = require('concat-stream');
+var marked = require('marked');
+var MarkedTerminal = require('marked-terminal');
+
+marked.setOptions({
+    renderer: new MarkedTerminal()
+});
+
+var moutput = through();
+var minput = concat({ encoding: 'string' }, function (res) {
+    moutput.end(marked(res));
+});
+var markdown = duplexer(minput, moutput);
 
 fs.createReadStream(__dirname + '/../readme.markdown')
-  .pipe(obj(function (chunk, enc, cb) {
-    this.push(msee.parse(chunk.toString()))
-  }))
+  .pipe(markdown)
   .pipe(pager());
